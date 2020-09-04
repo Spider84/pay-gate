4ba6a047f489535619522fde6f413b610a6e0b48
Alexey Belyaev <spider@spider.vc>
2020-09-04 13:26:15 +0600

    Опять имя модуля в импорте

--- pay_gate/__main__.py
+++ pay_gate/__main__.py
@@ -10,7 +10,7 @@ Why does this file exist, and why __main__? For more info, read:
 
 import sys
 
-from pay_gate import main
+from pay_gate.pay_gate import main
 
 if __name__ == '__main__':
     sys.exit(main())
